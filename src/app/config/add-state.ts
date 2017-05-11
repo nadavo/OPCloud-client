5a3f3da0c367f1cc3163c702b04753d76ee10de3
Oleg <olegzendel@campus.technion.ac.il>
2017-05-11 21:03:48 +0300

    Change all 'var' definitions to 'let'

--- src/app/config/add-state.ts
+++ src/app/config/add-state.ts
@@ -5,13 +5,13 @@ const paddingObject = 10;
 
 //Return true if the object contains a state which exceeds it (with a padding of paddingObject)
 function updateObject(fatherCell){
-  var leftSideX = fatherCell.get('originalPosition').x;
-  var topSideY = fatherCell.get('originalPosition').y;
-  var rightSideX = fatherCell.get('originalPosition').x + fatherCell.get('originalSize').width;
-  var bottomSideY = fatherCell.get('originalPosition').y + fatherCell.get('originalSize').height;
+  let leftSideX = fatherCell.get('originalPosition').x;
+  let topSideY = fatherCell.get('originalPosition').y;
+  let rightSideX = fatherCell.get('originalPosition').x + fatherCell.get('originalSize').width;
+  let bottomSideY = fatherCell.get('originalPosition').y + fatherCell.get('originalSize').height;
 
   _.each(fatherCell.getEmbeddedCells(), function(child) {
-    var childBbox = child.getBBox();
+    let childBbox = child.getBBox();
     //Updating the new size of the object to have margins of at least paddingObject so that the state will not touch the object
     if (childBbox.x <= (leftSideX+paddingObject)) { leftSideX = childBbox.x-paddingObject; }
     if (childBbox.y <= (topSideY+paddingObject)) { topSideY = childBbox.y-paddingObject; }
@@ -25,16 +25,16 @@ function updateObject(fatherCell){
 
 export function addState () {
 
-  var options = this.options;
+  let options = this.options;
   //this.startBatch();
-  var fatherObject = options.cellView.model;
-  var defaultState = new joint.shapes.opm.StateNorm(basicDefinitions.defineState());
+  let fatherObject = options.cellView.model;
+  let defaultState = new joint.shapes.opm.StateNorm(basicDefinitions.defineState());
   fatherObject.embed(defaultState);     //makes the state stay in the bounds of the object
   options.graph.addCells([fatherObject, defaultState]);
 
   //Placing the new state. By default it is outside the object.
-  var xNewState = fatherObject.getBBox().center().x - basicDefinitions.stateWidth/2;
-  var yNewState = fatherObject.getBBox().y + fatherObject.getBBox().height - basicDefinitions.stateHeight - paddingObject;
+  let xNewState = fatherObject.getBBox().center().x - basicDefinitions.stateWidth/2;
+  let yNewState = fatherObject.getBBox().y + fatherObject.getBBox().height - basicDefinitions.stateHeight - paddingObject;
   if (fatherObject.get('embeds') && fatherObject.get('embeds').length){
     _.each(fatherObject.getEmbeddedCells(), function(child) {
       if (!fatherObject.getBBox().containsPoint(child.getBBox().origin()) ||
@@ -50,9 +50,9 @@ export function addState () {
     cell.set('originalSize', cell.get('size'));
     cell.set('originalPosition', cell.get('position'));
 
-    var parentId = cell.get('parent');
+    let parentId = cell.get('parent');
     if (parentId){
-      var parent = options.graph.getCell(parentId);
+      let parent = options.graph.getCell(parentId);
       if (!parent.get('originalPosition')) parent.set('originalPosition', parent.get('position'));
       if (!parent.get('originalSize')) parent.set('originalSize', parent.get('size'));
       updateObject(parent);
