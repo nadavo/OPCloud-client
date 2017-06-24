088d37b548bfc685942ff08ed2565cfaaa5d93dd
samehmust <samehm@campus.technion.ac.il>
2017-06-24 22:11:01 +0300

    Add files via upload

---
+++ src/app/models/node.model.ts
@@ -0,0 +1,29 @@
+/**
+ * Created by sameh14 on 5/26/2017.
+ */
+export class Node {
+  id: string = '';
+  name: string = '';
+  className: string = 'root-class';
+  expanded: boolean = true;
+  isHidden: boolean = false;
+  hasChildren: boolean = false;
+  hasParent: boolean = false;
+  parent: string;
+  children: Node[] = [];
+  constructor(node: any) {
+    this.id = node.id;
+    this.name = node.name || '';
+    this.className = node.className || '';
+    this.expanded = node.expanded || false;
+    this.isHidden = node.isHidden || false;
+    this.hasParent = node.hasParent || false;
+    this.parent = node.parent || 'SD';
+    this.hasChildren = node.hasChildren || false;
+    this.children = node.children || [];
+  }
+
+  public addChildren(children) {
+    this.children.push(children);
+  }
+}
