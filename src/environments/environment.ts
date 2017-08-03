3cd56d05839d765157c9b67baf15c6803136ccb4
Shmuela <shmuela@500tech.com>
2017-08-03 19:06:10 +0300

    move firebase config to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,12 @@
 export const environment = {
   production: false,
   firebaseCredentials: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: 'AIzaSyDCcM4LU4GtKEAUtpW-xjfL1Vq9G8K0J2w',
+    authDomain: 'opcloud-client.firebaseapp.com',
+    databaseURL: 'https://opcloud-client.firebaseio.com',
+    projectId: 'opcloud-client',
+    storageBucket: 'opcloud-client.appspot.com',
+    messagingSenderId: '926349859368'
   },
   firebaseAuthProviders: [
     'google'
