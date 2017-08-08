dca04a0bc16dc3f61a02b685efc9c435ee34bfea
Shmuela <shmuela@500tech.com>
2017-08-08 15:03:24 +0300

    add environment config for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseCredentials: {
+    apiKey: 'AIzaSyDCcM4LU4GtKEAUtpW-xjfL1Vq9G8K0J2w',
+    authDomain: 'opcloud-client.firebaseapp.com',
+    databaseURL: 'https://opcloud-client.firebaseio.com',
+    projectId: 'opcloud-client',
+    storageBucket: 'opcloud-client.appspot.com',
+    messagingSenderId: '926349859368'
+  },
+  firebaseAuthProviders: [
+    'google'
+  ]
 };
