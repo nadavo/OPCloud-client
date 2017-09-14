33d2df5a503dd6991c93e315e3907b674e53e43e
natali.levi <soskin.natali@gmail.com>
2017-09-14 11:09:01 +0300

    firebase opcloud-sandbox deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
   firebaseCredentials: {
     apiKey: 'AIzaSyDCcM4LU4GtKEAUtpW-xjfL1Vq9G8K0J2w',
-    authDomain: 'opcloud-client.firebaseapp.com',
-    databaseURL: 'https://opcloud-client.firebaseio.com',
-    projectId: 'opcloud-client',
-    storageBucket: 'opcloud-client.appspot.com',
+    authDomain: 'opcloud-sandbox.firebaseapp.com',
+    databaseURL: 'https://opcloud-sandbox.firebaseio.com',
+    projectId: 'opcloud-sandbox',
+    storageBucket: 'opcloud-sandbox.appspot.com',
     messagingSenderId: '926349859368'
   },
   firebaseAuthProviders: [
