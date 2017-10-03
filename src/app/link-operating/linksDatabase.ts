ec48386724b3ec4fc1be96e48cd89fc8db9092a4
hanankohen <11sttNG11>
2017-10-03 14:20:12 +0300

    Updated overtime and undertime exception links

--- src/app/link-operating/linksDatabase.ts
+++ src/app/link-operating/linksDatabase.ts
@@ -101,6 +101,9 @@ export class dataBase {
       {sourceType: "object1.state", targetType1: "object1",targetType2: "null", linkName: "Undertime_exception"},
 
       {sourceType: "process1", targetType1: "process2",targetType2: "null", linkName: "Undertime_and_overtime_exception"},
+      {sourceType: "object1.state", targetType1: "process2",targetType2: "null", linkName: "Undertime_and_overtime_exception"},
+      {sourceType: "object1.state1", targetType1: "object1.state2",targetType2: "null", linkName: "Undertime_and_overtime_exception"},
+      {sourceType: "object1.state", targetType1: "object1",targetType2: "null", linkName: "Undertime_and_overtime_exception"},
 
       {sourceType: "object1", targetType1: "process2",targetType2: "null", linkName: "Condition_Consumption"},
       {sourceType: "object1.state", targetType1: "process2",targetType2: "null", linkName: "Condition_Consumption"},
